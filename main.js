34dcc17b55c5e0fcde26f19d67108b99880645b1
Roy <b.roynaim@gmail.com>
2021-03-30 00:06:35 +0200

    milestone 1, 2 and 3 updated

--- main.js
+++ main.js
@@ -1,8 +1,44 @@
 var app = new Vue({
   el: '#root',
   data: {
+    inputText: '',
     contacts: contacts,
+    user: [],
   },
+
+  // created: function () {
+  //
+  // },
+
+  methods: {
+    click_person: function (contact) {
+      this.user = contact,
+      console.log(this.user);
+    },
+
+    add: function (item) {
+      let today = new Date().toLocaleString();
+      if (this.inputText != '') {
+        let obj = {
+          text: this.inputText,
+          status: 'sent',
+          date: today,
+        }
+        item.messages.push(obj);
+        this.inputText = '';
+
+        setTimeout(function() {
+          let now = new Date().toLocaleString();
+          let answer = {
+            text: "Ok",
+            status: 'received',
+            date: now,
+          }
+          item.messages.push(answer);
+        }, 1000);
+      }
+    }
+  }
   // methods: {
   //   getTime: function(date) {
   //     let datetime = new Date(date);
