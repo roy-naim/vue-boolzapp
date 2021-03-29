8fb0faf828ab2a7b80a7b0bfa3e260edec57e58a
Roy <b.roynaim@gmail.com>
2021-03-29 16:58:21 +0200

    milestone 1

--- main.js
+++ main.js
@@ -2,5 +2,12 @@ var app = new Vue({
   el: '#root',
   data: {
     contacts: contacts,
-  }
+  },
+  // methods: {
+  //   getTime: function(date) {
+  //     let datetime = new Date(date);
+  //     let hours = datetime.getHours();
+  //     let minutes = datetime.getMinutes();
+  //     return: `${hours}:${minutes}`;
+  //   }
 });
